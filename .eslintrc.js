8bad6adaf404f83bb3a190ed48e8eaf23d34b039
andy-h1 <a.huynh2612@gmail.com>
2020-12-12 17:22:52 +0000

    feat(app): first commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:prettier/recommended', 'prettier/react'],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'jsx-a11y/href-no-hash': ['off'],
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'max-len': [
+      'warn',
+      {
+        code: 120,
+        tabWidth: 2,
+        comments: 120,
+        ignoreComments: false,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+  },
+};
