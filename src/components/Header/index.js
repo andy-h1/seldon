e3b9519572eb9cc0300f34e0ca660b4f4474bb23
andy-h1 <a.huynh2612@gmail.com>
2020-12-21 17:14:32 +0000

    style(header): fixing layout of header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 import { NavBar } from '../NavBar';
 import { DropdownMenu } from '../DropdownMenu';
-import * as S from './styles';
+// import * as S from './styles';
+
+const data = {
+  name: 'Date',
+  values: {
+    one: 'December',
+    two: 'November',
+    three: 'October',
+  },
+};
 
 export const Header = () => {
   return (
-    <S.HeaderWrapper>
-      <NavBar />
-      <DropdownMenu />
-    </S.HeaderWrapper>
+    <Grid container direction="row" justify="space-between" alignItems="stretch">
+      <Grid item xs={10} mr={1}>
+        <NavBar />
+      </Grid>
+      <Grid item xs={2}>
+        <DropdownMenu props={data} />
+      </Grid>
+    </Grid>
   );
 };
