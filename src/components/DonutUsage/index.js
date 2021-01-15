901f86f846703bd4b0592724686088aa366e8fe9
andy-h1 <a.huynh2612@gmail.com>
2021-01-15 00:20:18 +0000

    chore: removing duplicate code

--- src/components/DonutUsage/index.js
+++ src/components/DonutUsage/index.js
@@ -3,13 +3,14 @@ import { shape, string, number } from 'prop-types';
 import { Typography } from '@material-ui/core';
 import styled, { keyframes } from 'styled-components';
 import * as S from './styles';
+import { colours } from '../../tokens';
 
 const animate = (props) => keyframes`
 0% {
-  stroke-dashoffset: ${props.perimeter};
+  stroke-dashoffset: ${props.circumference};
 }
 100% {
-  stroke-dashoffset: ${props.perimeterGap};
+  stroke-dashoffset: ${props.circumferenceGap};
 }
 `;
 
@@ -23,30 +24,36 @@ const Circle = styled.circle`
   animation: ${animate} 1.5s;
   fill: transparent;
   stroke: url(#donutFill);
-  stroke-dasharray: ${({ perimeter }) => perimeter};
-  stroke-dashoffset: ${(props) => props.perimeterGap};
+  stroke-dasharray: ${({ circumference }) => circumference};
+  stroke-dashoffset: ${(props) => props.circumferenceGap};
   stroke-linecap: round;
   stroke-width: 5;
+`;
+
+export const FullCircle = styled.circle`
+  fill: transparent;
+  stroke-width: 5;
+  stroke: ${colours.lightGrey};
 `;
 
 export const DonutUsage = ({ data }) => {
   const { name, usage, total, color } = data;
   const radius = 45;
   const percentage = usage / total;
-  const perimeter = 2 * Math.PI * radius;
-  const perimeterGap = perimeter - perimeter * percentage;
+  const circumference = 2 * Math.PI * radius;
+  const circumferenceGap = circumference - circumference * percentage;
 
   return (
     <S.SVGWrapper>
       <svg width="100%" height="100%" viewBox="0 0 100 100">
-        <S.FullCircle cx={50} cy={50} r={radius} transform="rotate (-90 50 50)" />
+        <FullCircle cx={50} cy={50} r={radius} transform="rotate (-90 50 50)" />
         <Circle
           color={color}
           cx={50}
           cy={50}
           r={radius}
-          perimeter={perimeter}
-          perimeterGap={perimeterGap}
+          circumference={circumference}
+          circumferenceGap={circumferenceGap}
           transform="rotate (-90 50 50)"
         />
         <Text x="50%" y="50%">
--- src/components/DonutUsage/styles.js
+++ src/components/DonutUsage/styles.js
@@ -1,36 +1,4 @@
-import styled, { keyframes } from 'styled-components';
-import { colours } from '../../tokens';
-
-const animate = (props) => keyframes`
-  0% {
-    stroke-dashoffset: ${props.perimeter};
-  }
-  100% {
-    stroke-dashoffset: ${props.perimeterGap};
-  }
-`;
-
-export const Circle = styled.circle`
-  animation: ${animate} 1.5s;
-  fill: transparent;
-  stroke: ${({ color }) => color};
-  stroke-dasharray: ${({ perimeter }) => perimeter};
-  stroke-dashoffset: ${(props) => props.perimeterGap};
-  stroke-linecap: round;
-  stroke-width: 5;
-`;
-
-export const FullCircle = styled.circle`
-  fill: transparent;
-  stroke-width: 5;
-  stroke: ${colours.lightGrey};
-`;
-
-export const Text = styled.text`
-  dominant-baseline: middle;
-  text-anchor: middle;
-  fill: ${colours.white};
-`;
+import styled from 'styled-components';
 
 export const SVGWrapper = styled.div`
   height: 200px;
